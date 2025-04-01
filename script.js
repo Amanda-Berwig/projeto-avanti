cfbbb5f4047f186275a291dace5dd69796d117fa
Amanda Berwig <amandaberwig@gmail.com>
2025-04-01 19:48:16 -0300

    Adiciona estrutura do swiper com breakpoints para ajudar na responsividade

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 5,
   spaceBetween: 20,
-
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
   // If we need pagination
   pagination: {
     el: ".swiper-pagination-bullets",
